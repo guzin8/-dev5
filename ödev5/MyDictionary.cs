8d1c989fcd4515c833c8f7ee9379c5192265f85a
guzin8 <kanburogluguzin@gmail.com>
2021-01-21 21:16:41 -0800

    Add project files.

---
+++ ödev5/MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ödev5
+{
+    class MyDictionary<Key, Value>
+    {
+        Key[] keys;
+        Value[] values;
+        public MyDictionary()
+        {
+            keys = new Key[0];
+            values = new Value[0];
+        }
+        public void Add(Key k, Value v)
+        {
+            Key[] tempKeys = keys;
+            Value[] tempValues = values;
+            keys = new Key[keys.Length + 1];
+            values = new Value[values.Length + 1];
+
+            for(int i =0; i< tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+
+            keys[keys.Length - 1] = k;
+
+            for(int i = 0;i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+            values[values.Length - 1] = v;
+        }
+
+        public int Count()
+        {
+            return keys.Length;
+        }
+    }
+}
---
+++ ödev5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ödev5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> kelimeler = new MyDictionary<int, string>();
+            kelimeler.Add(1, "elma");
+            kelimeler.Add(2, "kitap");
+            kelimeler.Add(3, "silgi");
+            Console.WriteLine(kelimeler.Count());
+        }
+    }
+}
